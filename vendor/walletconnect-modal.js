7d9e24007447ff7db6adb060bf5ea724ac0cad07
Egor <begor8@gmail.com>
2025-11-21 18:02:59 +0800

    Replace WalletConnect with stable UMD build; fix modal in Mini App

--- vendor/walletconnect-modal.js
+++
@@ -1,7 +0,0 @@
-// walletconnect-modal.js
-// Thin ESM wrapper around WalletConnect Modal v2
-
-// If you want to pin another version – change @2.7.2 below.
-import { WalletConnectModal } from "https://cdn.skypack.dev/@walletconnect/modal@2.7.2";
-
-export { WalletConnectModal };
--- vendor/walletconnect/wc-sdk.js
+++
@@ -1,7 +0,0 @@
-// wc-sdk.js
-// Thin ESM wrapper around WalletConnect SignClient v2
-
-// If you want to pin another version – change @2.11.2 below.
-import SignClient from "https://cdn.skypack.dev/@walletconnect/sign-client@2.11.2";
-
-export default SignClient;
