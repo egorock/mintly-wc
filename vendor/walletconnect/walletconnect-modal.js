169a1e49a6de133f9d40af9ff96068e1bd698794
Egor <begor8@gmail.com>
2025-11-21 17:29:28 +0800

    Use CDN ESM wrappers for WalletConnect SignClient and Modal

--- vendor/walletconnect/walletconnect-modal.js
+++ vendor/walletconnect/walletconnect-modal.js
@@ -1,2 +1,7 @@
-export { W as WalletConnectModal } from './bundle-6897c25a.js';
-//# sourceMappingURL=bundle.js.map
+// walletconnect-modal.js
+// Thin ESM wrapper around WalletConnect Modal v2
+
+// If you want to pin another version – change @2.7.2 below.
+import { WalletConnectModal } from "https://cdn.skypack.dev/@walletconnect/modal@2.7.2";
+
+export { WalletConnectModal };
